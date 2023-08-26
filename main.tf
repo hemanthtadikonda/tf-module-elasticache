4cb13a0e16d7969569f21135f6eec359f675f5d7
hemanth <hemanthtadikonda29@gmail.com>
2023-08-26 14:57:20 +0530

    it is security_group_ids you put vpc_security_group_ids

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_elasticache_cluster" "main" {
   engine_version         = var.engine_version
   port                   = var.dbport
   subnet_group_name      = aws_elasticache_subnet_group.main.name
-  vpc_security_group_ids = [aws_security_group.main.id]
+  security_group_ids     = [aws_security_group.main.id]
   tags                   = merge( local.tags, { Name = "${local.name_prefix}-cluster" } )
 }
 
