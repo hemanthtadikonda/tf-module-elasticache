ca83f6479d03b8d8b2de02838fa7bf5e6c9b37ad
hemanth <hemanthtadikonda29@gmail.com>
2023-08-26 14:37:54 +0530

    tf - module - elasticache

---
+++ locals.tf
@@ -0,0 +1,5 @@
+locals {
+
+  name_prefix = "${var.env}-elasticache"
+  tags        = merge( var.tags , { tf-module = elasticache } , {env = var.env } )
+}
---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable "env" {}
+variable "tags" {}
+variable "subnet_ids" {}
+variable "pg_family" {}
+variable "engine" {}
+variable "node_type" {}
+variable "num_cache_nodes" {}
+variable "engine_version" {}
+variable "dbport" {}
+variable "sg_ingress_cidr_blocks" {}
+variable "vpc_id" {}
