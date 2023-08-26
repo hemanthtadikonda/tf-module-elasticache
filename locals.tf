3aecf48f440ccdd07a5e9543ab2d5a5cd30eb29d
hemanth <hemanthtadikonda29@gmail.com>
2023-08-26 14:53:40 +0530

    tf - module - elasticache

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
 
   name_prefix = "${var.env}-elasticache"
-  tags        = merge( var.tags , { tf-module = elasticache } , {env = var.env } )
+  tags        = merge( var.tags , { tf-module = "elasticache" } , {env = var.env } )
 }
